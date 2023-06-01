83f02dca0cfad5658d60e5c8312462a867579468
jovan-aleksic <jovandevelopersrb@gmail.com>
2023-06-02 00:43:38 +0400

    first upload

---
+++ android/app/src/main/kotlin/com/wallpaperapp/live/wallpapers/hd/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.wallpaperapp.live.wallpapers.hd
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/configs/config.dart
@@ -0,0 +1,35 @@
+
+
+class Config {
+  
+  final String appName = 'Walfy';
+  final String hashTag = '#walfy';
+  
+  final String packageName = 'mrblab.wallpaper_app';
+  final String appVersion = '3.1.0';
+
+  final String appIcon = 'assets/images/icon.png';
+  final String splashIcon = 'assets/images/splash.png';
+
+  
+  final String guestAvatar = 'assets/images/guest_avatar.png';
+
+  //Explore screen
+  static const String popularItemsName = 'Popular Walfies';
+  static const String newItemsName = 'New Walfies';
+
+  //Links
+  static const String privacyUrl = "https://mrb-lab.com/privacy-policy";
+
+  // -- Onesignal Push Notification
+  final String onesignalAppId = '4c4cac33-9cd3-42***************';
+  
+  //-- admob ads --
+  final String admobAppId = 'ca-app-pub-3940256099942544~3347511713';
+  final String admobInterstitialAdId = 'ca-app-pub-3940256099942544/1033173712';
+
+
+  //-- facebook ads--
+  //final String facebookInterstitialAdId = '587840845185406_587842688518555';
+
+}
